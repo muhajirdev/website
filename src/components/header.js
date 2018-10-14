c426b44063775d08da89c70edf13626ea4ed8e15
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-14 14:25:02 +0700

    Styles header

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,16 @@ import { Link } from 'gatsby'
 import styled from 'react-emotion'
 
 const Container = styled.div`
-  ${tw`bg-grey-lightest px-32 py-8`};
+  ${tw`bg-grey-lightest px-32 py-8 flex justify-between`};
+`
+const Items = styled.div`
+  ${tw`flex`};
+`
+const Item = styled.h3`
+  ${tw`text-grey-darkest ml-4 text-xl mb-0`};
 `
 const Title = styled.h1`
-  ${tw`text-grey-darkest m-0`};
+  ${tw`text-grey-darkest text-xl m-0`};
 `
 const StyledLink = styled(Link)`
   ${tw`no-underline text-inherit`};
@@ -14,9 +20,19 @@ const StyledLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <Container>
-    <StyledLink to="/">
-      <Title>{siteTitle}</Title>
-    </StyledLink>
+    <div>
+      <StyledLink to="/">
+        <Title>{siteTitle}</Title>
+      </StyledLink>
+    </div>
+    <Items>
+      <StyledLink to="/blog">
+        <Item>Blog</Item>
+      </StyledLink>
+      <StyledLink to="/projects">
+        <Item>Projects</Item>
+      </StyledLink>
+    </Items>
   </Container>
 )
 
