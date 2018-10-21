f11fd306e9fbc2197d5f397de9c947282b601d83
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-21 17:54:28 +0700

    Extract link to Link component

---
+++ src/components/Link.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { Link as GatsbyLink } from 'gatsby'
+
+import styled from 'react-emotion'
+
+const StyledLink = styled(GatsbyLink)`
+  ${tw`no-underline text-black`};
+`
+
+const StyledLinkWithAnchor = StyledLink.withComponent('a')
+
+const Link = ({ children, to, ...other }) => {
+  // Tailor the following test to your environment.
+  // This example assumes that any internal link (intended for Gatsby)
+  // will start with exactly one slash, and that anything else is external.
+  const internal = /^\/(?!\/)/.test(to)
+
+  // Use gatsby-link for internal links, and <a> for others
+  if (internal) {
+    return (
+      <StyledLink to={to} {...other}>
+        {children}
+      </StyledLink>
+    )
+  }
+  return (
+    <StyledLinkWithAnchor href={to} {...other}>
+      {children}
+    </StyledLinkWithAnchor>
+  )
+}
+
+export default Link
