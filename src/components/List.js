a77cd1d888a59d974de7b269c6842a800799f47c
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-26 09:37:38 +0700

    Update list component styling

--- src/components/List.js
+++ src/components/List.js
@@ -3,20 +3,24 @@ import styled from 'react-emotion'
 import Link from './Link'
 
 const Name = styled.div`
-  ${tw`font-bold text-grey-darkest py-2 mb-8 text-2xl`};
+  ${tw`font-bold text-grey-darkest py-2 text-2xl`};
+`
+
+const Container = styled.div`
+  ${tw`py-4`};
 `
 
 const Meta = styled.span`
   ${tw`text-grey text-xs`};
 `
 
-const List = ({ to, name, meta }) => 
-  (
-    <Link to={to}>
+const List = ({ to, name, meta }) => (
+  <Link to={to}>
+    <Container>
       <Name>{name}</Name>
       <Meta>{meta}</Meta>
-    </Link>
-  )
-
+    </Container>
+  </Link>
+)
 
 export default List
