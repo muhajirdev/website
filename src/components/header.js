f19aa50d85f7abae373b82c7023d061fc58c01fc
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-28 20:12:42 +0700

    Make logo responsive on mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import styled from 'react-emotion'
+import styled, { css } from 'react-emotion'
 import Link from './Link'
+
+const hideOnSmall = css`
+  ${tw`hidden lg:block`};
+`
+const hideOnBig = css`
+  ${tw`block lg:hidden`};
+`
 
 const Container = styled.div`
   ${tw`px-4 md:px-16 py-6 flex justify-between`};
@@ -12,17 +19,21 @@ const Item = styled.h3`
   ${tw`text-grey-darkest ml-4 sm:ml-8 text-xs mb-0`};
 `
 const Title = styled.h1`
-  ${tw`text-grey-darkest text-base m-0 hidden md:block`};
+  ${tw`text-grey-darkest text-base m-0`};
 `
 const StyledLink = styled(Link)`
   ${tw`no-underline text-inherit`};
 `
 
+const LogoBig = ({ title }) => <Title className={hideOnSmall}>{title}</Title>
+const LogoSmall = ({ title }) => <Title className={hideOnBig}>{title[0]}</Title>
+
 const Header = ({ siteTitle }) => (
   <Container>
     <div>
       <StyledLink to="/">
-        <Title>{siteTitle}</Title>
+        <LogoSmall title={siteTitle} />
+        <LogoBig title={siteTitle} />
       </StyledLink>
     </div>
     <Items>
