f4bc7f573c14d4d19caf4ec154d356d9d54a6145
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-28 21:32:55 +0700

    Create section small version

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,12 +4,21 @@ const darkBackground = css`
   ${tw`bg-grey-lighter`};
 `
 
+const paddingYBig = css`
+  ${tw`py-32`};
+`
+
+const paddingYSmall = css`
+  ${tw`py-12`};
+`
+
 const Section = styled.section`
-  ${tw`py-32 px-4 md:px-16`};
+  ${tw`px-4 md:px-16`};
   > * {
     ${tw`mx-auto max-w-xl`};
   }
   ${props => props.dark && darkBackground};
+  ${props => (props.small ? paddingYSmall : paddingYBig)};
 `
 
 export default Section
