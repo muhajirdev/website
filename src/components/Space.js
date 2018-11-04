dc41848c3bc373682253d6679d8edd134061d5fc
Muhammad Muhajir <muhajirframe@gmail.com>
2018-11-04 21:39:42 +0700

    Create Space.js

---
+++ src/components/Space.js
@@ -0,0 +1,10 @@
+import styled, { css } from 'react-emotion'
+
+const horizontalSpace = css`
+  ${tw`pl-4 sm:pl-8`};
+`
+
+const Space = styled.div`
+  ${props => props.horizontal && horizontalSpace};
+`
+export default Space
