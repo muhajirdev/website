bfec446035a3f54387627d33b491cc5682cf298e
Muhammad Muhajir <muhajirframe@gmail.com>
2018-11-04 23:52:12 +0700

    Add basic contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Section from '../components/Section'
+import { Tagline } from '../components/Text'
+import Link from '../components/Link'
+
+const Contact = () => (
+  <Section>
+    <Tagline>Contact</Tagline>
+    <div>
+      <Link to="mailto:muhajirframe@gmail.com">contact@muhajirframe.com</Link>
+    </div>
+  </Section>
+)
+
+export default Contact
