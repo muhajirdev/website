2e07978f7e4b6895110932a8b71ae28750e59e7a
Muhammad Muhajir <muhajirframe@gmail.com>
2018-11-07 08:46:03 +0700

    Style 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import Section from '../components/Section'
 import { Tagline } from '../components/Text'
-import Link from '../components/Link'
 
 const NotFoundPage = () => (
   <Section>
     <Tagline>NOT FOUND</Tagline>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <p>
-      Please email me{' '}
-      <Link to="mailto:contact@muhajirframe.com">contact@muhajirframe.com</Link>{' '}
-    </p>
   </Section>
 )
 
