7fe5cce47413440012ba76512367d43dcf9feb74
Muhammad Muhajir <muhajirframe@gmail.com>
2019-03-10 21:23:36 +0700

    feat(styles): add my and m util

--- src/styles.js
+++ src/styles.js
@@ -14,6 +14,32 @@ export const centerY = css(flex, {
   alignItems: 'center',
 })
 
+export const ml = arg =>
+  css({
+    marginLeft: arg,
+  })
+
+export const mr = arg =>
+  css({
+    marginRight: arg,
+  })
+
+export const mx = arg => css(ml(arg), mr(arg))
+
+export const mt = arg =>
+  css({
+    marginTop: arg,
+  })
+
+export const mb = arg =>
+  css({
+    marginBottom: arg,
+  })
+
+export const my = arg => css(mt(arg), mb(arg))
+
+export const m = arg => css(my(arg), mx(arg))
+
 /**
  *
  * @param {(string|number)} arg
