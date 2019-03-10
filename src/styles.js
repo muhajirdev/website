cd78c50ab625f508f318a9daa038b7778d2c7bf2
Muhammad Muhajir <muhajirframe@gmail.com>
2019-03-10 22:08:30 +0700

    feat(styles): add flexWrap

--- src/styles.js
+++ src/styles.js
@@ -6,6 +6,7 @@ export const name = '@muhajirdev/ui'
 export const flex = css({ display: 'flex' })
 export const centerX = css(flex, { justifyContent: 'center' })
 export const centerY = css(flex, { alignItems: 'center' })
+export const flexWrap = (arg = 'wrap') => css(flex, { flexWrap: arg })
 
 /* MARGIN */
 export const ml = arg => css({ marginLeft: arg })
