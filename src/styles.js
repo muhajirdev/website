34fcf5119a0d561d4fa7678ad3d88f91df9bb0dd
Muhammad Muhajir <muhajirframe@gmail.com>
2019-03-10 21:38:43 +0700

    refactor(styles): turn styles into one liner

--- src/styles.js
+++ src/styles.js
@@ -2,58 +2,25 @@ import { css } from '@emotion/core'
 
 export const name = '@muhajirdev/ui'
 
-export const flex = css({
-  display: 'flex',
-})
+/* FLEX */
+export const flex = css({ display: 'flex' })
+export const centerX = css(flex, { justifyContent: 'center' })
+export const centerY = css(flex, { alignItems: 'center' })
 
-export const centerX = css(flex, {
-  justifyContent: 'center',
-})
-
-export const centerY = css(flex, {
-  alignItems: 'center',
-})
-
-export const ml = arg =>
-  css({
-    marginLeft: arg,
-  })
-
-export const mr = arg =>
-  css({
-    marginRight: arg,
-  })
-
+/* MARGIN */
+export const ml = arg => css({ marginLeft: arg })
+export const mr = arg => css({ marginRight: arg })
+export const mt = arg => css({ marginTop: arg })
+export const mb = arg => css({ marginBottom: arg })
 export const mx = arg => css(ml(arg), mr(arg))
-
-export const mt = arg =>
-  css({
-    marginTop: arg,
-  })
-
-export const mb = arg =>
-  css({
-    marginBottom: arg,
-  })
-
 export const my = arg => css(mt(arg), mb(arg))
-
 export const m = arg => css(my(arg), mx(arg))
 
-/**
- *
- * @param {(string|number)} arg
- */
-export const width = arg =>
-  css({
-    width: arg,
-  })
-
+/* LENGTH */
+export const width = arg => css({ width: arg })
+export const maxWidth = arg => css({ maxWidth: arg })
 export const w = width
-
-export const maxWidth = arg =>
-  css({
-    maxWidth: arg,
-  })
-
 export const maxW = maxWidth
+
+/* Typography */
+export const lh = arg => css({ lineHeight: arg })
