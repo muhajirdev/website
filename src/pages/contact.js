4a9000ad795540d1b42d9bb4bc5ab9b2da5a00e4
Muhammad Muhajir <muhajirframe@gmail.com>
2019-03-17 17:14:37 +0700

    feat: add twitter DM button

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,42 @@
 import { useState } from 'react'
 import { navigateTo } from 'gatsby'
+import { css } from '@emotion/core'
 import Section from '../components/Section'
 import { Tagline } from '../components/Text'
 import { Input, TextArea } from '../components/Input'
 import Button from '../components/Button'
-import { flex, mq, flexCol, my, w } from '../styles'
+import {
+  mq,
+  flexCol,
+  my,
+  w,
+  link,
+  mb,
+  centerY,
+  mr,
+  fw,
+  flexWrap,
+  inlineFlex,
+  rounded,
+  px,
+  py,
+} from '../styles'
 import { margin } from '../../tailwind'
+import twitterIcon from '../images/twitter.svg'
 
 function encode(data) {
   return Object.keys(data)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
     .join('&')
 }
+
+const column = [w('100%'), mq('md')(w('50%'))]
+const border = css({ border: '2px solid #000' })
+
+const getTwitterDMUrl = id =>
+  `https://twitter.com/messages/compose?recipient_id=${id}`
+const TWITTER_ID = '1102551936060932098'
+const twitterDMUrl = getTwitterDMUrl(TWITTER_ID)
 
 const Contact = () => {
   const [name, setName] = useState()
@@ -38,8 +63,33 @@ const Contact = () => {
   return (
     <Section>
       <Tagline>Contact</Tagline>
-      <div css={flex}>
-        <div css={[w('100%'), mq('md')(w('50%'))]}>
+      <div css={flexWrap()}>
+        <div css={column}>
+          <a
+            href={twitterDMUrl}
+            css={[
+              link,
+              my(margin[8]),
+              centerY,
+              border,
+              rounded,
+              inlineFlex,
+              py(8),
+              px(16),
+            ]}
+          >
+            <img
+              alt="twitter icon"
+              src={twitterIcon}
+              css={[mb(0), w(48), mr(8)]}
+            />
+            <div>
+              <h3 css={mb(4)}>Twitter</h3>
+              <h6 css={[mb(0), fw(500)]}>Send me a DM</h6>
+            </div>
+          </a>
+        </div>
+        <div css={column}>
           <form
             css={[flexCol, my(margin[8]), w('100%')]}
             name="contact"
